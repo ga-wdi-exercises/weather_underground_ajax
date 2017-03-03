fc5aa2d39e2f37f04ca9ac991eb12b3d158b75bf
Nayana Davis <nayana487@gmail.com>
2017-03-03 08:11:30 -0500

    added stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+$("button").on("click", () => {
+  let url = "http://api.wunderground.com/api/91a02357a7011de7/conditions/q/CA/San_Francisco.json"
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType:"json"
+  }).done((response) => {
+    console.log(response.current_observation.temp_f)
+  }).fail(() => {
+    console.log("Ajax request fails")
+  }).always(() => {
+    console.log("This always happens regardless")
+  })
+})
